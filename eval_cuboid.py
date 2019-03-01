744c1039245b3050114a851b487e3f619119cccc
sunset <chengsun@gapp.nthu.edu.tw>
2019-03-01 17:26:58 +0800

    Add inference.py

---
+++ eval_cuboid.py
@@ -0,0 +1,195 @@
+import os
+import json
+import glob
+import argparse
+import numpy as np
+from tqdm import tqdm
+from scipy.spatial import HalfspaceIntersection
+from scipy.spatial import ConvexHull
+
+from misc import post_proc, panostretch
+
+
+def tri2halfspace(pa, pb, p):
+    ''' Helper function for evaluating 3DIoU '''
+    v1 = pa - p
+    v2 = pb - p
+    vn = np.cross(v1, v2)
+    if -vn @ p > 0:
+        vn = -vn
+    return [*vn, -vn @ p]
+
+
+def xyzlst2halfspaces(xyz_floor, xyz_ceil):
+    '''
+    Helper function for evaluating 3DIoU
+    return halfspace enclose (0, 0, 0)
+    '''
+    N = xyz_floor.shape[0]
+    halfspaces = []
+    for i in range(N):
+        last_i = (i - 1 + N) % N
+        next_i = (i + 1) % N
+
+        p_floor_a = xyz_floor[last_i]
+        p_floor_b = xyz_floor[next_i]
+        p_floor = xyz_floor[i]
+        p_ceil_a = xyz_ceil[last_i]
+        p_ceil_b = xyz_ceil[next_i]
+        p_ceil = xyz_ceil[i]
+        halfspaces.append(tri2halfspace(p_floor_a, p_floor_b, p_floor))
+        halfspaces.append(tri2halfspace(p_floor_a, p_ceil, p_floor))
+        halfspaces.append(tri2halfspace(p_ceil, p_floor_b, p_floor))
+        halfspaces.append(tri2halfspace(p_ceil_a, p_ceil_b, p_ceil))
+        halfspaces.append(tri2halfspace(p_ceil_a, p_floor, p_ceil))
+        halfspaces.append(tri2halfspace(p_floor, p_ceil_b, p_ceil))
+    return np.array(halfspaces)
+
+
+def eval_3diou(dt_floor_coor, dt_ceil_coor, gt_floor_coor, gt_ceil_coor, ch=-1.6,
+               coorW=1024, coorH=512, floorW=1024, floorH=512):
+    ''' Evaluate 3D IoU using halfspace intersection '''
+    dt_floor_coor = np.array(dt_floor_coor)
+    dt_ceil_coor = np.array(dt_ceil_coor)
+    gt_floor_coor = np.array(gt_floor_coor)
+    gt_ceil_coor = np.array(gt_ceil_coor)
+    assert (dt_floor_coor[:, 0] != dt_ceil_coor[:, 0]).sum() == 0
+    assert (gt_floor_coor[:, 0] != gt_ceil_coor[:, 0]).sum() == 0
+    N = len(dt_floor_coor)
+    dt_floor_xyz = np.hstack([
+        post_proc.np_coor2xy(dt_floor_coor, ch, coorW, coorH, floorW=1, floorH=1),
+        np.zeros((N, 1)) + ch,
+    ])
+    gt_floor_xyz = np.hstack([
+        post_proc.np_coor2xy(gt_floor_coor, ch, coorW, coorH, floorW=1, floorH=1),
+        np.zeros((N, 1)) + ch,
+    ])
+    dt_c = np.sqrt((dt_floor_xyz[:, :2] ** 2).sum(1))
+    gt_c = np.sqrt((gt_floor_xyz[:, :2] ** 2).sum(1))
+    dt_v2 = post_proc.np_coory2v(dt_ceil_coor[:, 1], coorH)
+    gt_v2 = post_proc.np_coory2v(gt_ceil_coor[:, 1], coorH)
+    dt_ceil_z = dt_c * np.tan(dt_v2)
+    gt_ceil_z = gt_c * np.tan(gt_v2)
+
+    dt_ceil_xyz = dt_floor_xyz.copy()
+    dt_ceil_xyz[:, 2] = dt_ceil_z
+    gt_ceil_xyz = gt_floor_xyz.copy()
+    gt_ceil_xyz[:, 2] = gt_ceil_z
+
+    dt_halfspaces = xyzlst2halfspaces(dt_floor_xyz, dt_ceil_xyz)
+    gt_halfspaces = xyzlst2halfspaces(gt_floor_xyz, gt_ceil_xyz)
+
+    in_halfspaces = HalfspaceIntersection(np.concatenate([dt_halfspaces, gt_halfspaces]),
+                                          np.zeros(3))
+    dt_halfspaces = HalfspaceIntersection(dt_halfspaces, np.zeros(3))
+    gt_halfspaces = HalfspaceIntersection(gt_halfspaces, np.zeros(3))
+
+    in_volume = ConvexHull(in_halfspaces.intersections).volume
+    dt_volume = ConvexHull(dt_halfspaces.intersections).volume
+    gt_volume = ConvexHull(gt_halfspaces.intersections).volume
+    un_volume = dt_volume + gt_volume - in_volume
+
+    return 100 * in_volume / un_volume
+
+
+def test(dt_cor_id, z0, z1, gt_cor_id, w, h, losses):
+    # Eval corner error
+    mse = np.sqrt(((gt_cor_id - dt_cor_id)**2).sum(1)).mean()
+    ce_loss = 100 * mse / np.sqrt(w**2 + h**2)
+
+    # Pixel surface error (3 labels: ceiling, wall, floor)
+    y0_dt = np.zeros(w)
+    y0_gt = np.zeros(w)
+    y1_gt = np.zeros(w)
+    for j in range(4):
+        coorxy = panostretch.pano_connect_points(dt_cor_id[j * 2],
+                                                 dt_cor_id[(j * 2 + 2) % 8],
+                                                 -z0)
+        y0_dt[np.round(coorxy[:, 0]).astype(int)] = coorxy[:, 1]
+
+        coorxy = panostretch.pano_connect_points(gt_cor_id[j * 2],
+                                                 gt_cor_id[(j * 2 + 2) % 8],
+                                                 -z0)
+        y0_gt[np.round(coorxy[:, 0]).astype(int)] = coorxy[:, 1]
+
+        coorxy = panostretch.pano_connect_points(gt_cor_id[j * 2 + 1],
+                                                 gt_cor_id[(j * 2 + 3) % 8],
+                                                 z0)
+        y1_gt[np.round(coorxy[:, 0]).astype(int)] = coorxy[:, 1]
+    assert (y0_dt == 0).sum() == 0
+    y1_dt = post_proc.infer_coory(y0_dt, z1 - z0, z0)
+
+    surface = np.zeros((h, w), dtype=np.int32)
+    surface[np.round(y0_dt).astype(int), np.arange(w)] = 1
+    surface[np.round(y1_dt).astype(int), np.arange(w)] = 1
+    surface = np.cumsum(surface, axis=0)
+    surface_gt = np.zeros((h, w), dtype=np.int32)
+    surface_gt[np.round(y0_gt).astype(int), np.arange(w)] = 1
+    surface_gt[np.round(y1_gt).astype(int), np.arange(w)] = 1
+    surface_gt = np.cumsum(surface_gt, axis=0)
+
+    pe_loss = 100 * (surface != surface_gt).sum() / (h * w)
+
+    # Eval 3d IoU
+    iou3d = eval_3diou(dt_cor_id[1::2], dt_cor_id[0::2], gt_cor_id[1::2], gt_cor_id[0::2])
+
+    losses['CE'].append(ce_loss)
+    losses['PE'].append(pe_loss)
+    losses['3DIoU'].append(iou3d)
+
+
+def prepare_gtdt_pairs(gt_glob, dt_glob):
+    gt_paths = sorted(glob.glob(gt_glob))
+    dt_paths = dict([(os.path.split(v)[-1].split('.')[0], v)
+                     for v in glob.glob(dt_glob)])
+
+    gtdt_pairs = []
+    for gt_path in gt_paths:
+        k = os.path.split(gt_path)[-1].split('.')[0]
+        if k in dt_paths:
+            gtdt_pairs.append((gt_path, dt_paths[k]))
+
+    return gtdt_pairs
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--dt_glob', required=True,
+                        help='NOTE: Remeber to quote your glob path.'
+                             'Files assumed to be json from inference.py')
+    parser.add_argument('--gt_glob', default='data/test/label_cor/*txt',
+                        help='NOTE: Remeber to quote your glob path.'
+                             'Files assumed to be txt')
+    parser.add_argument('--w', default=1024, type=int,
+                        help='GT images width')
+    parser.add_argument('--h', default=512, type=int,
+                        help='GT images height')
+    args = parser.parse_args()
+
+    # Prepare (gt, dt) pairs
+    gtdt_pairs = prepare_gtdt_pairs(args.gt_glob, args.dt_glob)
+
+    # Testing
+    losses = {
+        'CE': [],
+        'PE': [],
+        '3DIoU': [],
+    }
+    for gt_path, dt_path in tqdm(gtdt_pairs, desc='Testing'):
+        with open(gt_path) as f:
+            gt_cor_id = np.array([l.split() for l in f], np.float32)
+
+        with open(dt_path) as f:
+            dt = json.load(f)
+        dt_cor_id = np.array(dt['uv'], np.float32)
+        dt_cor_id[:, 0] *= args.w
+        dt_cor_id[:, 1] *= args.h
+
+        test(dt_cor_id, dt['z0'], dt['z1'], gt_cor_id, args.w, args.h, losses)
+
+    print(' Testing Result '.center(50, '='))
+    print('Corner Error (%):', np.mean(losses['CE']))
+    print('Pixel  Error (%):', np.mean(losses['PE']))
+    print('3DIoU        (%):', np.mean(losses['3DIoU']))
+    print('=' * 50)
---
+++ inference.py
@@ -0,0 +1,166 @@
+import os
+import glob
+import json
+import argparse
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from model import HorizonNet
+from misc import post_proc, panostretch, utils
+
+
+def find_4_peaks(signal):
+    W = signal.shape[0]
+    assert W % 4 == 0
+    signal_part = np.stack(np.split(signal, 4))
+    pk_loc = signal_part.argmax(1)
+    pk_loc += np.arange(4) * (W // 4)
+    return pk_loc, signal[pk_loc]
+
+
+def augment(x_img, flip, rotate):
+    x_img = x_img.numpy()
+    aug_type = ['']
+    x_imgs_augmented = [x_img]
+    if flip:
+        aug_type.append('flip')
+        x_imgs_augmented.append(np.flip(x_img, axis=-1))
+    for shift_p in rotate:
+        shift = int(round(shift_p * x_img.shape[-1]))
+        aug_type.append('rotate %d' % shift)
+        x_imgs_augmented.append(np.roll(x_img, shift, axis=-1))
+    return torch.FloatTensor(np.concatenate(x_imgs_augmented, 0)), aug_type
+
+
+def augment_undo(x_imgs_augmented, aug_type):
+    x_imgs_augmented = x_imgs_augmented.cpu().numpy()
+    sz = x_imgs_augmented.shape[0] // len(aug_type)
+    x_imgs = []
+    for i, aug in enumerate(aug_type):
+        x_img = x_imgs_augmented[i*sz : (i+1)*sz]
+        if aug == 'flip':
+            x_imgs.append(np.flip(x_img, axis=-1))
+        elif aug.startswith('rotate'):
+            shift = int(aug.split()[-1])
+            x_imgs.append(np.roll(x_img, -shift, axis=-1))
+        elif aug == '':
+            x_imgs.append(x_img)
+        else:
+            raise NotImplementedError()
+
+    return np.array(x_imgs)
+
+
+def inference(net, x, device, flip=False, rotate=[]):
+    '''
+    net   : the trained HorizonNet
+    x     : tensor in shape [1, 3, 512, 1024]
+    flip  : fliping testing augmentation
+    rotate: horizontal rotation testing augmentation
+    '''
+
+    H, W = tuple(x.shape[2:])
+
+    # Network feedforward (with testing augmentation)
+    x, aug_type = augment(x, args.flip, args.rotate)
+    y_bon_, y_cor_ = net(x.to(device))
+    y_bon_ = augment_undo(y_bon_.cpu(), aug_type).mean(0)
+    y_bon_ = (y_bon_ / np.pi + 0.5) * H - 0.5
+    y_cor_ = augment_undo(torch.sigmoid(y_cor_).cpu(), aug_type).mean(0)
+
+    y_bon_ = y_bon_[0]
+    y_cor_ = y_cor_[0, 0]
+
+    # Detech wall-wall peaks (cuboid version)
+    xs_ = find_4_peaks(y_cor_)[0]
+
+    # Init floor/ceil plane
+    z0 = 50
+    _, z1 = post_proc.np_refine_by_fix_z(*y_bon_, z0)
+
+    # Init manhattan aligned cuboid
+    cor, _ = post_proc.init_cuboid(xs_, y_bon_[0], z0, tol=abs(0.16 * z1 / 1.6))
+
+    # Expand with btn coory
+    cor = np.hstack([cor, post_proc.infer_coory(cor[:, 1], z1 - z0, z0)[:, None]])
+
+    # Collect corner position in equirectangular
+    cor_id = np.zeros((8, 2), np.float32)
+    for j in range(4):
+        cor_id[j*2] = cor[j, 0], cor[j, 1]
+        cor_id[j*2 + 1] = cor[j, 0], cor[j, 2]
+
+    # Normalized to [0, 1]
+    cor_id[:, 0] /= W
+    cor_id[:, 1] /= H
+
+    return cor_id, z0, z1
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--pth', required=True,
+                        help='path to load saved checkpoint.')
+    parser.add_argument('--img_glob', required=True,
+                        help='NOTE: Remeber to quote your glob path. '
+                             'All the given images are assumed to be aligned'
+                             'or you should use preporcess.py to do so.')
+    parser.add_argument('--output_dir', required=True)
+    # Augmentation related
+    parser.add_argument('--flip', action='store_true',
+                        help='whether to perfome left-right flip. '
+                             '# of input x2.')
+    parser.add_argument('--rotate', nargs='*', default=[], type=float,
+                        help='whether to perfome horizontal rotate. '
+                             'each elements indicate fraction of image width. '
+                             '# of input xlen(rotate).')
+    # Misc arguments
+    parser.add_argument('--no_cuda', action='store_true',
+                        help='disable cuda')
+    args = parser.parse_args()
+
+    # Prepare image to processed
+    paths = sorted(glob.glob(args.img_glob))
+    if len(paths) == 0:
+        print('no images found')
+    for path in paths:
+        assert os.path.isfile(path), '%s not found' % path
+
+    # Check target directory
+    if not os.path.isdir(args.output_dir):
+        print('Output directory %s not existed. Create one.')
+        os.makedirs(args.output_dir)
+    device = torch.device('cpu' if args.no_cuda else 'cuda')
+
+    # Loaded trained model
+    net = utils.load_trained_model(HorizonNet, args.pth).to(device)
+    net.eval()
+
+    # Inferencing
+    with torch.no_grad():
+        for i_path in tqdm(paths, desc='Inferencing'):
+            k = os.path.split(i_path)[-1]
+
+            # Load image
+            img_pil = Image.open(i_path)
+            if img_pil.size != (1024, 512):
+                img_pil = img_pil.resize((1024, 512), Image.BICUBIC)
+            img_ori = np.array(img_pil)[..., :3].transpose([2, 0, 1]).copy()
+            x = torch.FloatTensor([img_ori / 255])
+
+            # Inferenceing corners
+            cor_id, z0, z1 = inference(net, x, device, args.flip, args.rotate)
+
+            # Output result
+            with open(os.path.join(args.output_dir, k + '.json'), 'w') as f:
+                json.dump({
+                    'z0': float(z0),
+                    'z1': float(z1),
+                    'uv': [[float(u), float(v)] for u, v in cor_id],
+                }, f)
