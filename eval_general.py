1584daab88d0c2b80b877dca434ebc334bd82546
Cheng <chengsun@gapp.nthu.edu.tw>
2019-11-26 16:39:34 +0800

    Fix bug in layout_2_depth

--- eval_general.py
+++ eval_general.py
@@ -12,6 +12,7 @@ from misc import post_proc
 
 
 def layout_2_depth(cor_id, h, w):
+    # Convert corners to per-column boundary first
     # Up -pi/2,  Down pi/2
     vc, vf = cor_2_1d(cor_id, h, w)
     vc = vc[None, :]  # [1, w]
@@ -19,24 +20,34 @@ def layout_2_depth(cor_id, h, w):
     assert (vc > 0).sum() == 0
     assert (vf < 0).sum() == 0
 
-    # Per-pixel
+    # Per-pixel v coordinate (vertical angle)
     vs = ((np.arange(h) + 0.5) / h - 0.5) * np.pi
     vs = np.repeat(vs[:, None], w, axis=1)  # [h, w]
+
+    # Floor-plane to depth
+    floor_h = 1.6
+    floor_d = np.abs(floor_h / np.sin(vs))
+
+    # wall to camera distance on horizontal plane at cross camera center
+    cs = floor_h / np.tan(vf)
+
+    # Ceiling-plane to depth
+    ceil_h = np.abs(cs * np.tan(vc))      # [1, w]
+    ceil_d = np.abs(ceil_h / np.sin(vs))  # [h, w]
+
+    # Wall to depth
+    wall_d = np.abs(cs / np.cos(vs))  # [h, w]
+
+    # Recover layout depth
+    floor_mask = (vs > vf)
+    ceil_mask = (vs < vc)
+    wall_mask = (~floor_mask) & (~ceil_mask)
     depth = np.zeros([h, w], np.float32)    # [h, w]
+    depth[floor_mask] = floor_d[floor_mask]
+    depth[ceil_mask] = ceil_d[ceil_mask]
+    depth[wall_mask] = wall_d[wall_mask]
 
-    # Floor depth
-    floor_d = 1.6
-    depth[vs > vf] = floor_d
-
-    # Ceiling depth
-    ceil_d = np.abs(1.6 / np.tan(vf) * np.tan(vc))  # [1, w]
-    ceil_d = np.repeat(ceil_d, h, axis=0)           # [h, w]
-    depth[vs < vc] = ceil_d[vs < vc]
-
-    # Wall depth
-    wall_d = np.abs(1.6 / np.tan(vf) * np.tan(vs))  # [h, w]
-    depth[depth == 0] = wall_d[depth == 0]
-
+    assert (depth == 0).sum() == 0
     return depth
 
 
