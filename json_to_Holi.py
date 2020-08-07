12311bc426bc9ce08f21fedd1400fcd5612cbdb4
Cheng Sun <s2821d3721@gmail.com>
2020-08-08 00:12:55 +0800

    Update json_to_Holi

--- json_to_Holi.py
+++ json_to_Holi.py
@@ -6,6 +6,39 @@ from shapely.geometry import LineString
 
 from misc import panostretch
 
+def cor_2_1d(cor, H=512, W=1024):
+    bon_ceil_x, bon_ceil_y = [], []
+    bon_floor_x, bon_floor_y = [], []
+    n_cor = len(cor)
+    for i in range(n_cor // 2):
+        xys = panostretch.pano_connect_points(cor[i*2],
+                                              cor[(i*2+2) % n_cor],
+                                              z=-50, w=W, h=H)
+        bon_ceil_x.extend(xys[:, 0])
+        bon_ceil_y.extend(xys[:, 1])
+    for i in range(n_cor // 2):
+        xys = panostretch.pano_connect_points(cor[i*2+1],
+                                              cor[(i*2+3) % n_cor],
+                                              z=50, w=W, h=H)
+        bon_floor_x.extend(xys[:, 0])
+        bon_floor_y.extend(xys[:, 1])
+    bon_ceil_x, bon_ceil_y = sort_xy_filter_unique(bon_ceil_x, bon_ceil_y, y_small_first=True)
+    bon_floor_x, bon_floor_y = sort_xy_filter_unique(bon_floor_x, bon_floor_y, y_small_first=False)
+    bon = np.zeros((2, W))
+    bon[0] = np.interp(np.arange(W), bon_ceil_x, bon_ceil_y, period=W)
+    bon[1] = np.interp(np.arange(W), bon_floor_x, bon_floor_y, period=W)
+    #bon = ((bon + 0.5) / H - 0.5) * np.pi
+    return bon
+
+def sort_xy_filter_unique(xs, ys, y_small_first=True):
+    xs, ys = np.array(xs), np.array(ys)
+    idx_sort = np.argsort(xs + ys / ys.max() * (int(y_small_first)*2-1))
+    xs, ys = xs[idx_sort], ys[idx_sort]
+    _, idx_unique = np.unique(xs, return_index=True)
+    xs, ys = xs[idx_unique], ys[idx_unique]
+    assert np.all(np.diff(xs) > 0)
+    return xs, ys
+
 def find_occlusion(coor):
     u = panostretch.coorx2u(coor[:, 0])
     v = panostretch.coory2v(coor[:, 1])
@@ -33,26 +66,87 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     os.makedirs(args.output_dir, exist_ok=True)
-    os.makedirs(os.path.join(args.output_dir, 'v0'), exist_ok=True)
 
     paths = glob.glob(args.ori_glob)
     for path in paths:
-        with open(path) as f:
-            dt = json.load(f)
-        cor = np.array(dt['uv'], np.float32)
-        cor[:, 0] *= 1024
-        cor[:, 1] *= 512
+        if path.endswith('json'):
+            with open(path) as f:
+                dt = json.load(f)
+            cor = np.array(dt['uv'], np.float32)
+            cor[:, 0] *= 1024
+            cor[:, 1] *= 512
+        else:
+            with open(path) as f:
+                cor = np.array([l.strip().split() for l in f]).astype(np.float32)
+        cor = cor.reshape(-1, 4)
+        duplicated = [False] * len(cor)
+        for i in range(len(duplicated)):
+            for j in range(i+1, len(duplicated)):
+                if (cor[j] ==  cor[i]).sum() == 4:
+                    duplicated[j] = True
+        cor = cor[~np.array(duplicated)].reshape(-1, 2)
         cor = np.roll(cor[:, :2], -2 * np.argmin(cor[::2, 0]), 0)
         occlusion = find_occlusion(cor[::2].copy()).repeat(2)
 
+        '''
         cor_v0 = cor[~occlusion]
         cor_v0 = np.roll(cor_v0[:, :2], -2 * np.argmin(cor_v0[::2, 0]), 0)
         if (np.diff(cor_v0[:, 0]) < 0).sum():
             cor_v0[2::2] = np.flip(cor_v0[2::2], 0)
             cor_v0[3::2] = np.flip(cor_v0[3::2], 0)
-        with open(os.path.join(args.output_dir, 'v0', f'{os.path.split(path)[1][:-5]}.txt'), 'w') as f:
+        with open(os.path.join(args.output_dir, f'{os.path.split(path)[1][:-4]}.txt'), 'w') as f:
             for u, v in cor_v0:
                 f.write(f'{u:.0f} {v:.0f}\n')
-        # if occlusion.sum() == 2:
-        #     print(occlusion.sum())
+        '''
+
+        bon = cor_2_1d(cor)
+
+        cor_v1 = []
+        for i in range(0, len(cor), 2):
+            if occlusion[i] & ~occlusion[(i+2) % len(cor)]:
+                cur_x = cor[i, 0]
+                next_x = cor[(i+2) % len(cor), 0]
+                prev_x, j = None, i-2
+                while prev_x is None:
+                    if j < 0:
+                        j += len(cor)
+                    if ~occlusion[j]:
+                        prev_x = cor[j, 0]
+                        break
+                    j -= 2
+                dist2next = min(abs(next_x-cur_x), abs(next_x+1024-cur_x), abs(next_x-1024-cur_x))
+                dist2prev = min(abs(prev_x-cur_x), abs(prev_x+1024-cur_x), abs(prev_x-1024-cur_x))
+                # print(cor[i], prev_x, next_x, dist2next, dist2prev)
+                if dist2prev < dist2next:
+                    cor_v1.append([prev_x, bon[0, (int(prev_x)+1) % 1024]])
+                    cor_v1.append([prev_x, bon[1, (int(prev_x)+1) % 1024]])
+                else:
+                    cor_v1.append([next_x, bon[0, (int(next_x)-1) % 1024]])
+                    cor_v1.append([next_x, bon[1, (int(next_x)-1) % 1024]])
+            elif ~occlusion[i]:
+                cor_v1.extend(cor[i:i+2])
+
+        cor_v1 = np.stack(cor_v1, 0)
+        for _ in range(len(cor_v1)):
+            if np.alltrue(cor_v1[::2, 0][1:] - cor_v1[::2, 0][:-1] >= 0):
+                break
+            cor_v1 = np.roll(cor_v1, 2, axis=0)
+        if not np.alltrue(cor_v1[::2, 0][1:] - cor_v1[::2, 0][:-1] >= 0):
+            cor_v1[2::2] = np.flip(cor_v1[2::2], 0)
+            cor_v1[3::2] = np.flip(cor_v1[3::2], 0)
+        for _ in range(len(cor_v1)):
+            if np.alltrue(cor_v1[::2, 0][1:] - cor_v1[::2, 0][:-1] >= 0):
+                break
+            cor_v1 = np.roll(cor_v1, 2, axis=0)
+        if not np.alltrue(cor_v1[::2, 0][1:] - cor_v1[::2, 0][:-1] >= 0):
+            import pdb; pdb.set_trace()
+        #cor_v1 = np.roll(cor_v1[:, :2], -2 * np.argmin(cor_v1[::2, 0]), 0)
+        '''
+        if (np.diff(cor_v1[:, 0]) < 0).sum():
+            cor_v1[2::2] = np.flip(cor_v1[2::2], 0)
+            cor_v1[3::2] = np.flip(cor_v1[3::2], 0)
+        '''
+        with open(os.path.join(args.output_dir, f'{os.path.split(path)[1].replace("json", "txt")}'), 'w') as f:
+            for u, v in cor_v1:
+                f.write(f'{u:.0f} {v:.0f}\n')
 
